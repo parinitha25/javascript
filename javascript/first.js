92c285f1b40b8dd0fdc43f0569738eaf17e53623
parinitha25 <pnarayanaostb1@gmail.com>
2019-05-20 16:31:02 +0530

    task1

---
+++ javascript/first.js
@@ -0,0 +1,15 @@
+var a='welcome to "javascript"';
+document.write(a);
+
+
+var x=10;
+var y=12;
+var z=x+y;
+console.log(z);
+
+var name='swapna';
+var age=22;
+window.alert(name+''+age);
+
+var p=10;
+document.write(p);
---
+++ javascript/second.js
@@ -0,0 +1,5 @@
+var z='hello world', a='welcome',b='javascript';
+var e='hi good afternoon';
+var g=' had your launch';
+document.write(z+"<br>"+a+"<br>"+b+"<br>");
+document.write(e+''+g);
