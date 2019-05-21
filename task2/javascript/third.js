17220a146bb4aee23d7b3aac122e284ab038de7b
parinitha25 <pnarayanaostb1@gmail.com>
2019-05-21 16:55:13 +0530

    second day task

---
+++ task2/javascript/third.js
@@ -0,0 +1,25 @@
+
+		// var number='firstno',c='=';
+		
+		// var num='secondno';
+		// var firstno=10;
+		// var secondno=20;s
+		// document.write(c);
+		// document.write(firstno);
+		// document.write("<br>");
+		// document.write(num);
+		// document.write(c);
+		// document.write(secondno);
+		// document.write("<br>");
+
+		var one=10;
+		var two=20;
+		var add=one+two;
+		document.write('firstno'+ "="+one+"<br>"+'secondno'+"="+two+"<br>");
+		document.write(one + "+" + two + "="+ add);
+		var sub=one-two;
+		document.write("<br>"+one + "-" + two + "="+ sub);
+		var mul=one*two;
+		document.write("<br>"+one + "*" + two + "="+ mul);
+		var div=one/two;
+		document.write("<br>"+one + "/" + two + "="+ div);
